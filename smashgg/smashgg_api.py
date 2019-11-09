3240c189a8fb77639ca0f6be3a2bb6d41bb9d642
James Roumeliotis <james.roumeliotis@cimpress.com>
2019-11-09 16:00:05 -0500

    adding new files for abstract apis

---
+++ smashgg/smashgg_api.py
@@ -0,0 +1,13 @@
+import requests
+
+def executeApi( query, variables, api_token ) :
+    """
+    """
+
+    url = 'https://api.smash.gg/gql/alpha'
+    json = { 'query' : '%s' % query, 'variables' : '%s' % variables }
+    headers = {'Authorization': 'Bearer %s' % api_token}
+
+    res = requests.post(url=url, json=json, headers=headers).json()
+
+    return res
